9e8d392fa09480f6e8b9a8a957805833a622c057
moamen <elwazanmoamen@gmail.com>
2023-07-14 13:18:33 -0700

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: smile in the mirrir
+ *
+ * Return: always 0(success)
+ */
+int main(void)
+{
+	char lr = 'z';
+
+	while (lr <= 'a')
+	{
+		putchar(lr);
+		lr++;
+	}
+	putchar('\n');
+	return (0);
+}
