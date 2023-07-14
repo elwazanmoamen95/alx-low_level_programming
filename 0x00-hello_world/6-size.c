8a0c989ba94c82a09f09c040f69679eab35178a3
moamen <elwazanmoamen@gmail.com>
2023-07-13 18:40:06 -0700

    6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio>
+/**
+ * main : entry point
+ *
+ * Description: A c-program print sizeof
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+printf("size of a char: %ld byte(s)\n", sizeof(char));
+printf("size of an int: %ld byte(s)\n", sizeof(int));
+printf("size of a long int: %ld byte(s)\n", sizeof(long int));
+printf("size of a long long int: %ld byte(s)\n", sizeof(long long int));
+printf("size of a float: %ld byte(S)\n", sizeof(float));
+return (0);
+
+}
