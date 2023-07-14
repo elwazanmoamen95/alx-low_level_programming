2cc99006a9adcedb128ba4870e24c4b359cc2ff9
moamen <elwazanmoamen@gmail.com>
2023-07-14 13:06:22 -0700

    5

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.j>
+/**
+ * main - entry point
+ *
+ * Description: numberz
+ *
+ * Return: always 0(success)
+ */
+int main(void)
+{
+	int n = 48;
+
+	while (n <= 57)
+	{
+		putchar(48');
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
