5678978522516358d8b0c5900dd6fdef08560664
moamen <elwazanmoamen@gmail.com>
2023-07-14 18:47:09 -0700

    100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: different combinations of two digits.
+ *
+ * Return: always 0(success)
+ */
+int main(void)
+{
+	int a = 0, b;
+
+	while (a <= 9)
+	{
+		b = 0;
+		while (b <= 9)
+		{
+			if (a != b && a < b)
+			{
+				putchar(a + 48);
+				putchar(b + 48);
+
+				if (a + b != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			b++;
+		}
+		a++;
+	}
+	putchar('\n');
+
+	return (0);
+}
