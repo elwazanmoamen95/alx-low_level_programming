851d28630808d02fc0591f5297de3af7530aa438
moamen <elwazanmoamen@gmail.com>
2023-07-18 11:28:57 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Description: prints the alphabet, in lowercase
+ *
+ * Return: always 0(success)
+ */
+void ptint_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+}
