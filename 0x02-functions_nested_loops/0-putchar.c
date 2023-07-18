c72e95e1f99abd53951fa754e4b39ce3125f0346
moamen <elwazanmoamen@gmail.com>
2023-07-18 10:58:15 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <main.h>
+/**
+ * main - entry point
+ *
+ * Description: pritn _putchar
+ *
+ * Return: always 0(success)
+ */
+int main(void)
+{
+	char a[] = "_putchar";
+	int i;
+
+	for (i = 0; i <= 8; i++)
+	{
+		_putchar(a[i]);
+	}
+
+	-putchar('\n');
+
+	return (0);
+}
