6a27ec8097f7d2c38b7b69cca8c4cc7692e5c11d
moamen <elwazanmoamen@gmail.com>
2023-07-18 23:06:42 -0700

    4

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: computes the sum of all
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int sum, num;
+
+	for (num = 0; num <= 1024; num++)
+	{
+		if (num % 3 == 0 || num % 5 == 0)
+			sum += num;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
