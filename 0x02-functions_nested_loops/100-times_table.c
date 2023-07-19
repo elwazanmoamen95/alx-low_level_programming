ad1d1109957bb32dfb4759a034d7c8a96279e89f
moamen <elwazanmoamen@gmail.com>
2023-07-18 22:59:44 -0700

    4

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -32,7 +32,7 @@ void print_times_table(int n)
 				if (c > 99)
 				{
 					_putchar((c / 100) + '0');
-					_putchar((c % 100) + '0');
+					_putchar((c / 10) % 10 + '0');
 				}
 				_putchar((c % 10) + '0');
 
