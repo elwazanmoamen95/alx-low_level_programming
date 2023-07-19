1d5df65dd17e2fa8c79ad8af6f3fec8a9fc32ea9
moamen <elwazanmoamen@gmail.com>
2023-07-18 19:40:07 -0700

    9

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - add two integers
+ *
+ * @a: FIRST
+ * @b: second
+ *
+ * Retuen: sum of a & b 
+ */
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
