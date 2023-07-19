07a306aebf04421e4adc5b7acdef4bb6ff86691d
moamen <elwazanmoamen@gmail.com>
2023-07-18 23:38:44 -0700

    4

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Description: even liber abbaci
+ *
+ * Return: always 0(success)
+ */
+int main(void)
+{
+	long a = 0, b = 1, sum, totalsum;
+
+	while (true)
+	{
+		sum = a + b;
+
+		if ((sum % 2) == 0)
+			totalsum += sum;
+		if (sum > 4000000)
+			break;
+
+		a = b;
+		b = sum;
+	}
+	printf("%lu\n", totalsum);
+
+	return (0);
+}
