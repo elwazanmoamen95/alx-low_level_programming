6fc1d35a564a8008f6d0d0ec41c5b25314cb7ce4
moamen <elwazanmoamen@gmail.com>
2023-07-20 10:37:22 -0700

    0

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main"
+
+/**
+ * _isupper - check if c is upper
+ *
+ * @c: input for alphabet
+ *
+ * Return: 1 if its uppercase, o if not
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
