8b56757b8068d3a6308276af4a2dca1a3a560d48
moamen <elwazanmoamen@gmail.com>
2023-07-20 10:43:08 -0700

    0

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "mian.h"
+
+/**
+ * mut - multiplies two integer
+ *
+ * @a: first integer
+ * @b: second integer
+ * @prod: multiples of two integer
+ *
+ * Return: always 0 (success)
+ */
+int mul(int a, int b)
+{
+	prod = a * b;
+	return (prod);
+}
