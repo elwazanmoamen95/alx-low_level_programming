8bf5249e02619002f274d480586862a0b1f49891
moamen <elwazanmoamen@gmail.com>
2023-07-20 13:30:25 -0700

    0

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * more_numbers - prints 10 times from 0 to 14
+ *
+ * Return: always 0(success)
+ */
+void more_numbers(void)
+{
+	int i, n;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			if (n >= 10)
+			{
+				_putchar('1');
+				n = n % 10;
+			}
+			_putchar(n + '0');
+		}
+		_putchar('\n');
+	}
+}
