3d402aaed64ce25075d3cca40ec0f49b05d23aa3
moamen <elwazanmoamen@gmail.com>
2023-07-20 16:59:32 -0700

    0

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_diagonal - draws a diagonla lines
+ *
+ * @n: number of \
+ *
+ * Return: always 0(success)
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('\\');
+	}
+	_putchar('\n');
+}
