999479a5551d5f9b11e0453bdf3c042db56ffbfd
moamen <elwazanmoamen@gmail.com>
2023-07-20 17:38:03 -0700

    0

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Return: always 0(success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz  ");
+		if else (i % 5 == 0)
+			PRINTF("Buzz ");
+		if else (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else
+			printf("%d ", i);
+	}
+	return (0);
+}
