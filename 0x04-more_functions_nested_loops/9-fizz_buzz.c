c28a6120b2407206129e56cfd09acc0ba47aff0e
moamen <elwazanmoamen@gmail.com>
2023-07-20 17:45:09 -0700

    0

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,14 +10,15 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if ((i % 3 == 0) && !(i % 5 == 0))
 			printf("Fizz  ");
-		if else(i % 5 == 0)
+		if else((i % 5 == 0) && !(i % 3 == 0))
 			printf("Buzz ");
 		if else(i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz ");
 		else
 			printf("%d ", i);
 	}
+	printf("\n");
 	return (0);
 }
