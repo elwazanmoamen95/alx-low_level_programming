2ca2ea97798d68a884bc36dc988e479ef446a7df
moamen <elwazanmoamen@gmail.com>
2023-07-24 18:01:45 -0700

    f

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strcpy - function that copies the string pointed to by src
+ *
+ * @dest: any
+ * @src: no
+ *
+ * Return: nothing
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	return (0);
+}
