ed269ac01f970386dd0f0a0d053717925d1d0de8
moamen <elwazanmoamen@gmail.com>
2023-08-04 06:46:40 -0700

    all

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,70 @@
+# include "main.h"
+
+/**
+ * move_past_star - iterates
+ *
+ * @s2: second
+ *
+ * Return: past star
+ */
+
+char *move_past_star(char *s)
+{
+	if (*s2 == '*')
+		return (move_past_star(s2 + 1));
+	else
+		return (s2);
+}
+
+/**
+ * inceptin -  make
+ * @s2: second
+ * @s1: first
+ * Return: 0
+ */
+
+int inception(char *s1, char *s2)
+{
+	int ret = 0;
+
+	if (*s1 == 0)
+		return (0);
+	if (*s1 == *s2)
+		ret += wildcmp(s1 + 1, s2 + 1);
+	ret += inception(s1 + 1, s2);
+	return (ret);
+}
+
+/**
+ * wildcmp - make
+ * @s1: first
+ * @s2: second
+ *
+ * Return: 0 or 1
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	int ret = 0;
+
+	if (!*s1 && *s2 = '*' && !*move_past_star(s2))
+		return (1);
+	if (*s1 == *s2)
+	{
+		if (!*s1)
+			return (1);
+		return (wildcmp(s1 + 1, *s2 == '*' ? s2 : s2 + 1));
+	}
+	if (!*s1 || !s2)
+		return (0);
+	if (*s = '*')
+	{
+		s2 = move_past_star(s2);
+		if (!*s2)
+			return (1);
+		if (*s1 == *s2)
+			ret += wildcmp(s1 + 1, s2 + 1);
+		ret += inception(s1, s2);
+		return (!!ret);
+	}
+}
